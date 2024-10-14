5422e1ea1e45e017e28958e3afb7de455330e73b
Dejong1706 <ppk0320@gmail.com>
2024-10-14 16:21:59 +0900

    [create] 자주하는 질문 데이터 불러오기

--- Front/bookleum/components/notice/inquiry.tsx
+++ Front/bookleum/components/notice/inquiry.tsx
@@ -1,10 +1,65 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import Link from "next/link";
+import axios from "axios";
+import { formatDate } from "@/utils/formatDate";
+import { getCookie } from "cookies-next";
+
+interface Inquiry {
+  id: number;
+  title: string;
+  createdDate: string;
+}
 
 export default function Inquiry() {
+  const [inquiryList, setInquiryList] = useState([]);
+  const [userId, setUserId] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const id = getCookie("userId") as string | undefined;
+    if (id) {
+      setUserId(id);
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/user/inquiry/read/${userId}`
+        );
+        if (response.status === 200) {
+          console.log(response);
+          // const inquiryData = response.data.map((qustion: Inquiry) => {
+          //   return {
+          //     title: qustion.title,
+          //     id: qustion.id,
+          //     createdDate: qustion.createdDate,
+          //   };
+          // });
+          // setInquiryList(inquiryData);
+        } else {
+          console.error("데이터를 불러오지 못했습니다.");
+        }
+      } catch (error) {
+        console.error("서버 에러:", error);
+        alert("서버 에러 발생");
+      }
+    };
+
+    if (userId !== undefined) {
+      fetchData();
+    }
+  }, [userId]);
+
+  console.log(userId);
+
   return (
     <div className="w-[70%] mx-auto">
       <div>
-        <p className="pl-2 font-bold text-[2rem]">1:1문의</p>
+        <p className="pl-2 font-bold text-[2rem]">1:1문의 내역</p>
         <div className="w-full h-[4px] bg-black mt-2"></div>
         <ul className="flex items-center pl-2 pt-2">
           <li className="w-[20%]">No.</li>
@@ -13,13 +68,17 @@ export default function Inquiry() {
         </ul>
         <div className="w-full h-[2px] bg-black mt-2"></div>
       </div>
-      <div className="py-3">
-        <ul className="flex items-center pl-2 pt-2">
-          <li className="w-[20%]">1.</li>
-          <li className="w-[50%]">고객센터 휴무 안내</li>
-          <li className="w-[30%]">2024.09.19</li>
-        </ul>
-      </div>
+      {inquiryList.map((inquiry: Inquiry, index) => (
+        <div className="py-4" key={inquiry.id}>
+          <Link href={`/Other/notice/noticeDetail/${inquiry.id}`}>
+            <ul className="flex items-center pl-2 pt-2">
+              <li className="w-[20%]">{index + 1}.</li>
+              <li className="w-[50%]">{inquiry.title}</li>
+              <li className="w-[30%]">{formatDate(inquiry.createdDate)}</li>
+            </ul>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
--- Front/bookleum/components/notice/question.tsx
+++ Front/bookleum/components/notice/question.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import Link from "next/link";
+import axios from "axios";
+import { formatDate } from "@/utils/formatDate";
+
+interface Question {
+  id: number;
+  title: string;
+  createdDate: string;
+}
 
 export default function Question() {
+  const [questionList, setQuestionList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/user/question/read`
+        );
+        if (response.status === 200) {
+          console.log(response);
+          const qustionData = response.data.map((qustion: Question) => {
+            return {
+              title: qustion.title,
+              id: qustion.id,
+              createdDate: qustion.createdDate,
+            };
+          });
+          setQuestionList(qustionData);
+        } else {
+          console.error("데이터를 불러오지 못했습니다.");
+        }
+      } catch (error) {
+        console.error("서버 에러:", error);
+        alert("서버 에러 발생");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(questionList);
   return (
     <div className="w-[70%] mx-auto">
       <div>
-        <p className="pl-2 font-bold text-[2rem]">자주찾는 질문</p>
+        <p className="pl-2 font-bold text-[2rem]">자주하는 질문</p>
         <div className="w-full h-[4px] bg-black mt-2"></div>
         <ul className="flex items-center pl-2 pt-2">
           <li className="w-[20%]">No.</li>
@@ -13,13 +56,17 @@ export default function Question() {
         </ul>
         <div className="w-full h-[2px] bg-black mt-2"></div>
       </div>
-      <div className="py-3">
-        <ul className="flex items-center pl-2 pt-2">
-          <li className="w-[20%]">1.</li>
-          <li className="w-[50%]">고객센터 휴무 안내</li>
-          <li className="w-[30%]">2024.09.19</li>
-        </ul>
-      </div>
+      {questionList.map((question: Question, index) => (
+        <div className="py-4" key={question.id}>
+          <Link href={`/Other/notice/noticeDetail/${question.id}`}>
+            <ul className="flex items-center pl-2 pt-2">
+              <li className="w-[20%]">{index + 1}.</li>
+              <li className="w-[50%]">{question.title}</li>
+              <li className="w-[30%]">{formatDate(question.createdDate)}</li>
+            </ul>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
